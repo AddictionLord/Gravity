2dc35b3f3298060110a1d3668169bb57ab15dcaf
AddictionLord <marousiinek@gmail.com>
2021-10-18 16:01:01 +0200

    Function to compute velocity after collision in one dimension

--- include/gravity/physicsUtils.hpp
+++ include/gravity/physicsUtils.hpp
@@ -13,6 +13,17 @@ namespace physics
         return - (GRAVITY * attractorMass) / (std::pow(distance, 2)) * normalized_dir_vector;
     }
 
+    // Computes ub (velocity of object B in x axis after collision) 
+    inline auto computeVelocityAfterCollision(
+        const float& va,
+        const float& vb,
+        const float& ma,
+        const float& mb
+    ) -> float
+    {
+        return ((2 * va * ma) / (ma + mb)) + ((vb * (mb - ma)) / (mb + ma));
+    }
+
 
 //--------------------------------------------
     // inline auto isInCollision()
